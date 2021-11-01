ab0dfaf2dd0c5cf21b29ebf3e4c8b609ebe0e2fc
KakaoPiraten <heinonen.simon@gmail.com>
2021-11-01 14:02:00 +0100

    FirstCommit

---
+++ JS/UtilFunctions.js
@@ -0,0 +1,35 @@
+function keyPress()
+    {
+        let el = document.getElementsByName("inputfield2")[0];
+        el.classList.add("MyCssClass");
+        alert("you added a class to inputfield2");
+    }
+
+function validateInput()
+{
+    
+    let formValue = document.forms["myForm"]["myFormTextInput"].value;
+    if(formValue === "")
+    {
+        document.getElementById("errorMessage").innerHTML = "Error: input needs a value";
+    }
+    else{
+        document.getElementById("errorMessage").innerHTML = "";
+
+        // Post data to backend..
+    }
+}
+
+function drawMap(tileMap)
+{
+    
+    for(let x = 0; x < tileMap.width; x++)
+    {
+        for(let y = 0; y < tileMap.height; y ++)
+        {
+            console.log("x:" + x + " y:" + y);
+        }
+    }
+}
+
+drawMap(tileMap01);
